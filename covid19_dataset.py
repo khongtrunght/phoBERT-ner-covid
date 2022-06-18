7a209354e01ba1646f8b149cf14966304b6ea507
khongtrunght <khongtrunght@gmail.com>
2022-06-18 16:05:12 +0700

    first commit

---
+++ covid19_dataset.py
@@ -0,0 +1,191 @@
+from genericpath import exists
+import os
+from pathlib import Path
+import datasets
+
+
+logger = datasets.logging.get_logger(__name__)
+
+
+_CITATION = ""
+_DESCRIPTION = """\
+"""
+
+
+class COVID19Config(datasets.BuilderConfig):
+    """BuilderConfig for Covid19"""
+
+    def __init__(self, **kwargs):
+        """BuilderConfig for Covid19.
+
+        Args:
+        **kwargs: keyword arguments forwarded to super.
+        """
+        super(COVID19Config, self).__init__(**kwargs)
+
+
+class COVID19(datasets.GeneratorBasedBuilder):
+    """COVID19 NER dataset."""
+
+    BUILDER_CONFIGS = [
+        COVID19Config(name="PhoNER_COVID19", version=datasets.Version("1.0.0"),
+                      description="PhoNER_COVID19 dataset"),
+    ]
+
+    #PATIENT_ID, NAME, GENDER, AGE, JOB, LOCATION,ORGANIZATION, DATE, SYMPTOM_AND_DISEASE, TRANSPORTATION
+    def __init__(self,
+                 *args,
+                 cache_dir,
+                 url="https://raw.githubusercontent.com/VinAIResearch/PhoNER_COVID19/main/data/word/",
+                 train_file="train_word.conll",
+                 val_file="dev_word.conll",
+                 test_file="test_word.conll",
+                 ner_tags=('B-AGE',
+                           'B-DATE',
+                           'B-GENDER',
+                           'B-JOB',
+                           'B-LOCATION',
+                           'B-NAME',
+                           'B-ORGANIZATION',
+                           'B-PATIENT_ID',
+                           'B-SYMPTOM_AND_DISEASE',
+                           'B-TRANSPORTATION',
+                           'I-AGE',
+                           'I-DATE',
+                           'I-GENDER',
+                           'I-JOB',
+                           'I-LOCATION',
+                           'I-NAME',
+                           'I-ORGANIZATION',
+                           'I-PATIENT_ID',
+                           'I-SYMPTOM_AND_DISEASE',
+                           'I-TRANSPORTATION',
+                           'O'),
+                 **kwargs):
+        self._ner_tags = ner_tags
+        self._url = url
+        self._train_file = train_file
+        self._val_file = val_file
+        self._test_file = test_file
+        super(COVID19, self).__init__(*args, cache_dir=cache_dir, **kwargs)
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "id": datasets.Value("string"),
+                    "tokens": datasets.Sequence(datasets.Value("string")),
+                    "ner_tags": datasets.Sequence(
+                        datasets.features.ClassLabel(
+                            names=sorted(list(self._ner_tags))
+                        )
+                    )
+                }
+            ),
+            supervised_keys=None,
+            homepage="",
+            citation=_CITATION,
+
+        )
+
+    def _split_generators(self, dl_manager):
+        """Return SplitGenerators."""
+        urls_to_download = {
+            "train": f"{self._url}{self._train_file}",
+            "dev": f"{self._url}{self._val_file}",
+            "test": f"{self._url}{self._test_file}",
+        }
+        downloaded_files = dl_manager.download_and_extract(urls_to_download)
+
+        return [
+            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={
+                                    "filepath": downloaded_files["train"]}),
+            datasets.SplitGenerator(name=datasets.Split.VALIDATION, gen_kwargs={
+                                    "filepath": downloaded_files["dev"]}),
+            datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs={
+                                    "filepath": downloaded_files["test"]}),
+        ]
+
+    def _generate_examples(self, filepath):
+        logger.info("⏳ Generating examples from = %s", filepath)
+        with open(filepath, encoding="utf-8") as f:
+            guid = 0
+            tokens = []
+            ner_tags = []
+            for line in f:
+                if line == "" or line == "\n":
+                    if tokens:
+                        yield guid, {
+                            "id": str(guid),
+                            "tokens": tokens,
+                            "ner_tags": ner_tags,
+                        }
+                        guid += 1
+                        tokens = []
+                        ner_tags = []
+                else:
+                    # SROIE2019 tokens are space separated
+                    splits = line.split(" ")
+                    tokens.append(splits[0])
+                    ner_tags.append(splits[1].rstrip())
+            # last example
+            yield guid, {
+                "id": str(guid),
+                "tokens": tokens,
+                "ner_tags": ner_tags,
+            }
+
+
+class COVID19Dataset(object):
+    """
+    """
+    NAME = "PhoNER_COVID19"
+
+    def __init__(self):
+        cache_dir = os.path.join(str(Path.home()), '.phoner_covid19')
+        print("Cache dir: ", cache_dir)
+        os.makedirs(cache_dir, exist_ok=True)
+        download_config = datasets.DownloadConfig(cache_dir=cache_dir)
+        self._dataset = COVID19(cache_dir=cache_dir)
+        print("Cache1 dir:", self._dataset.cache_dir)
+        self._dataset.download_and_prepare(download_config=download_config)
+        self._dataset = self._dataset.as_dataset()
+
+    @property
+    def dataset(self):
+        return self._dataset
+
+    @property
+    def labels(self) -> datasets.ClassLabel:
+        return self._dataset['train'].features['ner_tags'].feature.names
+
+    @property
+    def id2label(self):
+        return dict(list(enumerate(self.labels)))
+
+    @property
+    def label2id(self):
+        return {v: k for k, v in self.id2label.items()}
+
+    def train(self):
+        return self._dataset['train']
+
+    def test(self):
+        return self._dataset['test']
+
+    def validation(self):
+        return self._dataset['validation']
+
+
+if __name__ == "__main__":
+    dataset = COVID19Dataset().dataset
+
+    print(dataset['train'])
+    print(dataset['test'])
+    print(dataset['validation'])
+
+    print("List of tags: ",
+          dataset['train'].features['ner_tags'].feature.names)
+
+    print("First sample: ", dataset['train'][0])
---
+++ main.py
@@ -0,0 +1,105 @@
+import numpy as np
+from hf_tokenize import HFTokenizer
+from transformers import AutoModelForTokenClassification, DataCollatorForTokenClassification, AutoTokenizer, TrainingArguments, Trainer
+from covid19_dataset import COVID19Dataset
+import os
+from datasets import load_metric
+
+
+metric = load_metric("seqeval")
+
+
+def compute_metrics(p, label_list):
+    predictions, labels = p
+    predictions = np.argmax(predictions, axis=2)
+
+    # Remove ignored index (special tokens)
+    true_predictions = [
+        [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+    true_labels = [
+        [label_list[l] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+
+    results = metric.compute(
+        predictions=true_predictions, references=true_labels)
+    return {
+        "precision": results["overall_precision"],
+        "recall": results["overall_recall"],
+        "f1": results["overall_f1"],
+        "accuracy": results["overall_accuracy"],
+    }
+
+
+model_n_version = "sroie2019v1"
+max_epochs = 50
+learning_rate = 2e-5
+batch_size = 16
+model_root_dir = "~/.phoner_covid19/models/hf/"
+
+hf_pretrained_tokenizer_checkpoint = "vinai/phobert-base"
+hf_dataset = COVID19Dataset()
+
+hf_model = AutoModelForTokenClassification.from_pretrained(
+    "vinai/phobert-base",
+    num_labels=len(hf_dataset.labels))
+
+hf_model.config.id2label = hf_dataset.id2label
+hf_model.config.label2id = hf_dataset.label2id
+
+hf_preprocessor = HFTokenizer.init_vf(
+    hf_pretrained_tokenizer_checkpoint=hf_pretrained_tokenizer_checkpoint)
+
+tokenized_datasets = hf_dataset.dataset.map(
+    hf_preprocessor.tokenize_and_align_labels, batched=True)
+
+
+data_collator = DataCollatorForTokenClassification(hf_preprocessor.tokenizer)
+
+
+args = TrainingArguments(
+    f"test-ner",
+    evaluation_strategy="epoch",
+    learning_rate=learning_rate,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=4,
+    weight_decay=0.01,
+)
+
+trainer = Trainer(
+    hf_model,
+    args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    tokenizer=hf_preprocessor.tokenizer,
+    compute_metrics=lambda p: compute_metrics(p=p, label_list=hf_dataset.labels)
+)
+
+trainer.train()
+trainer.evaluate()
+
+# Predictions on test dataset and evaluation
+
+predictions, labels, _ = trainer.predict(tokenized_datasets["test"])
+predictions = np.argmax(predictions, axis=2)
+
+# Remove ignored index (special tokens)
+true_predictions = [
+    [hf_dataset.labels[p] for (p, l) in zip(prediction, label) if l != -100]
+    for prediction, label in zip(predictions, labels)
+]
+
+true_labels = [
+    [hf_dataset.labels[l] for (p, l) in zip(prediction, label) if l != -100]
+    for prediction, label in zip(predictions, labels)
+]
+
+results = metric.compute(predictions=true_predictions, references=true_labels)
+print(results)
+
+out_dir = os.path.expanduser(model_root_dir) + "/" + model_n_version
+trainer.save_model(out_dir)
